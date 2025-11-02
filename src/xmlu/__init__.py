1f8cf7ac959726b4e10134c1414e960f63e9477a
Michiel Meire <michiel@Michiels-MacBook-Pro.local>
2025-11-02 15:56:46 +0100

    add typer cli app

--- src/xmlu/__init__.py
+++ src/xmlu/__init__.py
@@ -1,3 +1,11 @@
-from .main import main
+from .convert_to_pydantic import create_models_file, generate_pydantic_models
+from .main import app
+from .utils import convert_to_pascal_case, convert_to_snake_case
 
-__all__ = ["main"]
+__all__ = [
+    "app",
+    "create_models_file",
+    "generate_pydantic_models",
+    "convert_to_pascal_case",
+    "convert_to_snake_case",
+]
